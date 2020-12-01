37c167fd091a7edf6a6aa34836afb2a14002be72
nuttapong01 <69332738+nuttapong01@users.noreply.github.com>
2020-12-01 11:23:47 +0700

    Add files via upload

---
+++ es6-301.js
@@ -0,0 +1,17 @@
+//The String
+//1.The basic Syntax of String
+//let message = "Hello World!";
+//console.log(message);
+//2.The basic Syntax of String
+let fname = "Mark"
+let lname = "Sakaberg"
+let fullname = fname + " " + lname;
+//console.log(fname + lname);
+console.log(fullname);
+//3.The String with number
+console.log("Hello" + 12)//Hello12
+console.log("10" + 2)//102
+
+console.log("10" == 10);//true
+console.log("10" === 10);//false
+console.log("10" !== 10);//true
